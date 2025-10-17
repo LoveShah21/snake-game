2ba65cfc28ea230edf164ebc86edd325ea0aafb5
LoveShah21 <loveshah2112@gmail.com>
2025-10-17 19:10:58 +0530

    base classes added

--- game.cpp
+++ game.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+struct Point
+{
+    int x, y;
+
+    Point(int x = 0, int y = 0) : x(x), y(y) {}
+
+    bool operator==(const Point &other) const
+    {
+        return x == other.x && y == other.y;
+    }
+};
+
+// ============ Food Class ============
+class Food
+{
+    //    food execution
+};
+
+// ============ Snake Class ============
+class Snake
+{
+    // snake execution
+};
+
+// ============ Game Class ============
+class Game
+{
+    // game execution
+};
 int main()
 {
     cout << "Hello, Snake Game!" << endl;
