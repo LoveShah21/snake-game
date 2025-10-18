c93535ed3ef3b37a7693ec82beca3dcfbf424bd6
LoveShah21 <loveshah2112@gmail.com>
2025-10-18 20:11:49 +0530

    food class defined

--- game.cpp
+++ game.cpp
@@ -17,6 +17,37 @@ struct Point
 class Food
 {
     //    food execution
+private:
+    Point position;
+
+public:
+    Food() : position(0, 0) {}
+
+    void spawn(int gridWidth, int gridHeight, const deque<Point> &snakeBody)
+    {
+        bool validPosition;
+        do
+        {
+            validPosition = true;
+            position.x = rand() % gridWidth;
+            position.y = rand() % gridHeight;
+
+            // Ensure food doesn't spawn on snake
+            for (const auto &segment : snakeBody)
+            {
+                if (position == segment)
+                {
+                    validPosition = false;
+                    break;
+                }
+            }
+        } while (!validPosition);
+    }
+
+    Point getPosition() const
+    {
+        return position;
+    }
 };
 
 // ============ Snake Class ============
